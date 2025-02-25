43f537f6c04dd4b5aedcf9c922b1a780dcb6b7d0
Falos732 <alexyouellet1@gmail.com>
2025-02-25 14:44:06 -0500

    init

---
+++ script.js
@@ -0,0 +1,5 @@
+function Connect(){
+    var email = document.getElementsByName('login-email')[0].value;
+    var password = document.getElementsByName('login-password')[0].value;
+    alert("Email: " + email + "\nMot de passe: " + password);
+}
