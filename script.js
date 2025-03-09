6302bd97a3d1759f1db3ba1c89b07fd84063f7fc
Falos732 <alexyouellet1@gmail.com>
2025-03-09 16:33:34 -0400

    Tableau de bord commencer et j'ai tout renommer pour etre en francais (j'ai oublier de le push quand je l'ai fait ya 2 semaine)

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function Connect(){
     var email = document.getElementsByName('login-email')[0].value;
     var password = document.getElementsByName('login-password')[0].value;
     alert("Email: " + email + "\nMot de passe: " + password);
+    window.location.href = "./tableauDeBord.html";
 }
